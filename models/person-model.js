4f8ec68156c903776d8fa5c88df7c78e13e0078e
Trang Nguyen <trangnguyen80@gmail.com>
2017-11-04 10:08:24 +0100

    Add comments and fix syntax

--- models/person-model.js
+++ models/person-model.js
@@ -1,10 +1,17 @@
+/**
+ * @param  {number} id
+ * @param  {string} name
+ * @param  {number} age, default to 0
+ */
 module.exports = class Person {
-    constructor(id, name, age) {
+    // default param at the last position
+    constructor(id, name, age = 0) {
         this.id = id
         this.name = name
-        this.age = age || 0
+        this.age = age
     }
 
+    // create an instance of the class 
     static create(person) {
         return new Person(person.id, person.name, person.age);
     }
