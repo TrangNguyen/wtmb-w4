20b968ea589b10d620d71901ac67f4093b8f1a5d
Trang Nguyen <trangnguyen80@gmail.com>
2017-11-09 21:38:43 +0100

    Change person model to use mongoose model

--- models/person-model.js
+++ models/person-model.js
@@ -1,11 +1,26 @@
-module.exports = class Person {
-    constructor(id, name, age) {
-        this.id = id
-        this.name = name
-        this.age = age || 0
-    }
+// require mongoose to work with mongodb
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
-    static create(person) {
-        return new Person(person.id, person.name, person.age);
-    }
-}
+// declare a schema - blue print
+const PersonSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    }, // type of the value for this field
+    age: {
+        type: Number,
+        default: 0
+    },
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Person'
+    }]
+});
+
+// using mongose-sequence to auto increment the id field.
+PersonSchema.plugin(AutoIncrement, { inc_field: 'id' });
+
+// create/export a mongoose model with name and its schema.
+module.exports = mongoose.model('Person', PersonSchema); 
