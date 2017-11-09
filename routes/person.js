2a75ce0cb83abebad671b64a67d182c919fabe71
Trang Nguyen <trangnguyen80@gmail.com>
2017-11-09 21:39:36 +0100

    Allow adding friends

--- routes/person.js
+++ routes/person.js
@@ -24,6 +24,15 @@ router.post('/', async (req, res, next) => {
     res.send(person)
 })
 
+router.post('/:id/friends', async (req, res, next) => {
+    const person = await PersonService.find(req.params.id);
+    const friend = await PersonService.find(req.body.targetId);
+    // use mongodb addToSet to keep unique record
+    person.friends.addToSet(friend);
+    const updatedPerson = await person.save();
+    res.send(updatedPerson);
+})
+
 router.delete('/:id', async (req, res, next) => {
     await PersonService.del(req.params.id)
 
