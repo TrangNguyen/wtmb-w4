22902eb2808dbb00d447c37b4839869ba5946eea
Trang Nguyen <trangnguyen80@gmail.com>
2017-11-09 21:39:04 +0100

    Update person-service

--- services/person-service.js
+++ services/person-service.js
@@ -1,59 +1,20 @@
-const fs = require('fs')
-
-const PersonModel = require('../models/person-model')
-
-const dbPath = `${__dirname}/../database.json`
-
-async function findAll() {
-    return new Promise((resolve, reject) => {
-        fs.readFile(dbPath, 'utf8', (err, file) => {
-            if (err) return reject(err)
-
-            const people = JSON.parse(file).map(PersonModel.create)
-
-            resolve(people)
-        })
-    })
+const PersonModel = require('../models/person-model');
+// service is needed when we do more business logic than the db model provide.
+function findAll() {
+    return PersonModel.find(); // find all records in person model
 }
 
-async function add(person) {
-    const allPeople = await findAll()
-    const lastPerson = allPeople[allPeople.length - 1]
-    const lastPersonsId = lastPerson && lastPerson.id || 0
-    person.id = lastPersonsId + 1
-
-    person = PersonModel.create(person)
-    allPeople.push(person)
-
-    await saveAll(allPeople)
-
-    return person
+function add(person) {
+    return PersonModel.create(person);
 }
 
-async function del(personId) {
-    const allPeople = await findAll()
-    const personIndex = allPeople.findIndex(p => p.id == personId)
-    if (personIndex < 0) return
-
-    allPeople.splice(personIndex, 1)
-
-    saveAll(allPeople)
+function del(id) {
+    return PersonModel.remove({ id });
 }
 
-async function find(personId) {
-    const allPeople = await findAll()
-
-    return allPeople.find(p => p.id == personId)
-}
-
-async function saveAll(people) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(dbPath, JSON.stringify(people), (err, file) => {
-            if (err) return reject(err)
-
-            resolve()
-        })
-    })
+function find(id) {
+    // first record that matches
+    return PersonModel.findOne({id}).populate('friends');
 }
 
 module.exports = {
@@ -61,4 +22,4 @@ module.exports = {
     find,
     add,
     del
-}
+};
