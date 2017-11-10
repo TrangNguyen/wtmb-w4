5db3905f95b407e247308daf7254446d836bd3e5
Trang Nguyen <trangnguyen80@gmail.com>
2017-11-10 15:24:50 +0100

    Add a new database for whiteboard app

--- database-connection.js
+++ database-connection.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 //override mongoose's promise with the global one
 mongoose.Promise = global.Promise;
 // uri contains host and db name
-mongoose.connect('mongodb://localhost/wtmbjs', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/whiteboard', { useMongoClient: true });
